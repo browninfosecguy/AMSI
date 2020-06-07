434c70069e10053d0218df378c6d665c878790f0
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-06-07 12:36:46 -0300

    initial commit

---
+++ base64/base64/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText;
+            string encodedText;
+            string base64ToPlainText;
+
+            Console.WriteLine("Enter Text to Encode!");
+            plainText = Console.ReadLine();
+
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(plainText);
+
+            encodedText = System.Convert.ToBase64String(data);
+
+            Console.WriteLine("Encoded text " + encodedText);
+
+            byte[] data2 = System.Convert.FromBase64String(encodedText);
+
+
+            base64ToPlainText = System.Text.ASCIIEncoding.ASCII.GetString(data2);
+
+            Console.WriteLine("Decoded String " + base64ToPlainText);
+
+
+        }
+    }
+}
